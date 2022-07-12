3aed5405ff2d1e0b93bc44f6065e8b264f20e39a
DestinyIJ <ighedodestiny11@gmail.com>
2022-07-12 19:26:31 +0100

    deploy-to-heroku update

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 Route::post('/login', [AuthController::class, 'login']); // route to login
 Route::post('/logout', [AuthController::class, 'logout']); // route to logout
